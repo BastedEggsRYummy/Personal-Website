13d833c463e3655e2209d67017b2d935f0db90dc
BastedEggsRYummy <baimej@rpi.edu>
2024-12-28 15:48:23 -0500

    Made name component with scaling

--- jackson-baimel-site/src/App.js
+++ jackson-baimel-site/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      <Name />
+      <Name scale={2}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- jackson-baimel-site/src/components/name.js
+++ jackson-baimel-site/src/components/name.js
@@ -1,10 +1,22 @@
-import './name.css'
+import './name.css';
 
-function Name() {
-  return(
-    <>
-    <div id='name'>Jackson Baimel</div>
-    </>
+function Name({ scale = 1 }) {
+  return (
+    <div
+      id="scalable-container"
+      style={{
+        transform: `scale(${scale})`,
+        transformOrigin: 'center',
+      }}
+    >
+      <div id="name-component">
+        <div id="j-letter">J</div>
+        <div id="name-tagline-box">
+          <div id="name">ackson Baimel</div>
+          <div id="tagline">CS + IT Extraordinaire</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
